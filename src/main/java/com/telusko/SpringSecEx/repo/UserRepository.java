8da7bf1d85d4f2f6426a440ddf496272db99befa
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2025-07-09 09:24:46 +0530

    Revert "polish-one"
    This reverts commit 8199127dd6d226a1f3beda16ad7270c57c6eb210.

--- src/main/java/com/telusko/SpringSecEx/repo/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.vat.repository;
-
-import com.example.vat.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {}
--- src/main/java/com/telusko/SpringSecEx/repo/VatRegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.vat.repository;
-
-import com.example.vat.entity.VatRegistration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface VatRegistrationRepository extends JpaRepository<VatRegistration, Integer> {
-    Optional<VatRegistration> findByUserId(Integer userId);
-}
